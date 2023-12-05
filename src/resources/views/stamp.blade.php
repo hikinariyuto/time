dd2efdc1b19bac09e8cdfd19c729dde1f6f7c665
yuto <yutotoron2911@gmail.com>
2023-12-05 17:23:04 +0900

    任意のメッセージ

---
+++ src/resources/views/stamp.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="stamp.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="head">
+        <div class="atte">
+            <header>Atte</header>
+        </div>
+        <ul class="navi">
+                <li>ホーム</li>
+                <li>日付一覧</li>
+                <li>ログアウト</li>
+        </ul>
+    </div>
+    <div class="main">
+    <div class="greeting">
+        <p>太郎さんお疲れ様です！</p>
+    </div>
+    <diV class="work_time">
+        <diV class="work_start">
+            <p class="come">勤務開始</p>
+        </div>
+        <div class="work_finish">
+            <p class="home">勤務終了</p>
+        </div>
+    </div>
+    <div class="rest_time">
+        <diV class="rest_start">
+            <p class="tired">休憩開始</p>
+        </div>
+        <diV class="rest_finish">
+            <p class="refresh">休憩終了</p>
+        </div>
+    </div>
+</body>
+</html>
